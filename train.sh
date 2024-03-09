f93dbae5c7d21320d4c6f566bd0126cf8871c09a
Pika <danielho1671@gmail.com>
2024-03-10 04:39:30 +0800

    Minor update train config

--- train.sh
+++ train.sh
@@ -3,16 +3,18 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate MixDehazeNet
 
 # Set common variables
+SAVEPATH="./saved_models/ParametricAtten2/"
 DATAPATH="../datasets/data/"
 DATASET="RESIDE-6K"
 EXP="reside6k"
-GPUS="0,1"
+GPUS="0"
 
 # Create the text features for the dataset
-python clip_text.py --data_dir $DATAPATH --dataset $DATASET --gpu $GPUS
+# python clip_text.py --data_dir $DATAPATH --dataset $DATASET --gpu $GPUS
 
 # Train the model
 # Dataset directory: ../dataset/data/
-#python train.py --model MixDehazeNet-b --save_dir ./saved_models/indoor/ --dataset RESIDE-IN --exp indoor --gpu 2
-#python train.py --model MixDehazeNet-b --save_dir ./saved_models/outdoor/ --dataset RESIDE-OUT --exp outdoor --gpu 3
-python train.py --model MixDehazeNet-s --save_dir ./saved_models/reside6k/ --data_dir $DATAPATH --dataset $DATASET --exp $EXP --gpu $GPUS
+python train.py --model MixDehazeNet-s --save_dir $SAVEPATH --data_dir $DATAPATH --dataset $DATASET --exp $EXP --gpu $GPUS
+
+# Evaluate the model
+python test.py --model MixDehazeNet-s --save_dir $SAVEPATH --data_dir $DATAPATH --dataset $DATASET --exp $EXP --gpu $GPUS
